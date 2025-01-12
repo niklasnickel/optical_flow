07ced50041f68a62e899157968eafe64145d5d20
Niklas Nickel <niklas.nickel@rwth-aachen.de>
2025-01-12 16:04:24 +0100

    Change hs_method to Zack-Pock

--- hs.py
+++ hs.py
@@ -1,21 +1,39 @@
+#################################
+### Horn-Schunck Optical Flow ###
+#################################
+
+# Import the required libraries
 import cv2
 import numpy as np
 
 print("Installed opencv version", cv2.__version__)
 print("Installed numpy version", np.__version__)
 
-### Horn-Schunck Optical Flow ###
+#####################
+# Parameters
+#####################
 
-# Parameters for Farneback optical flow
-hs_params = dict(
-	pyr_scale=0.5,
-	levels=4,
-	winsize=15,
-	iterations=6,
-	poly_n=5,
-	poly_sigma=1.2,
-	flags=0
+# Parameters for the Zach-Pock implementation of the Horn-Schunck optical flow
+# See: https://docs.opencv.org/3.3.0/dc/d47/classcv_1_1DualTVL1OpticalFlow.html and https://link.springer.com/chapter/10.1007/978-3-540-74936-3_22
+params = dict(
+	# tau=0.125, # Time step of the numerical scheme
+	lambda_=0.10,  # Smoothness parameter (lower numbers are smoother)
+	nscales=5,  # Number of scales in the image pyramid
+	warps=5,  # Number of warps per scale .Represents the number of times that I1(x+u0) and grad(I1(x+u0)) are computed per scale.
+	epsilon=0.01,  # Stopping criterion threshold used in the numerical scheme
+	# scaleStep=0.8, # This is not documented well, I don't know what it does.
+	# gamma=0.0, #  This is not documented well, I don't know what it does.
+	medianFiltering=5,  # Size of the filter to be applied to the flow field at each pyramid level. Use 1 to disable filtering.
+	useInitialFlow=False,  # Use the input flow as an initial flow approximation. (False since we don't have an initial flow)
+
+	innnerIterations=30,  # Don't bother. Ask me if you are interested. ;)
+	outerIterations=10,  # Don't bother. Ask me if you are interested. ;)
+	theta = 0.3  # Don't bother. Ask me if you are interested. ;)
 )
+
+#####################
+# Preparation
+#####################
 
 vidPath = "testing_data/bamboo_1/clean.mp4"
 output_video_file = "output_video.mp4"
@@ -36,15 +54,35 @@ old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
-etime = 0
+frame_number = 0
+
+#####################
+# Tracking
+#####################
+
 while 1:
 	ret, frame = cap.read()
 	if frame is None:
 		break
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
+	print(f"Processing frame {frame_number}")
+
 	# calculate optical flow
-	flow = cv2.calcOpticalFlowFarneback(old_gray, frame_gray, None, **hs_params)
+	optical_flow = cv2.optflow.createOptFlow_DualTVL1()
+	# optical_flow.setTau(params['tau'])
+	optical_flow.setLambda(params['lambda_'])
+	optical_flow.setTheta(params['theta'])
+	optical_flow.setScalesNumber(params['nscales'])
+	optical_flow.setWarpingsNumber(params['warps'])
+	optical_flow.setEpsilon(params['epsilon'])
+	optical_flow.setInnerIterations(params['innnerIterations'])
+	optical_flow.setOuterIterations(params['outerIterations'])
+	# optical_flow.setScaleStep(params['scaleStep'])
+	# optical_flow.setGamma(params['gamma'])
+	optical_flow.setMedianFiltering(params['medianFiltering'])
+	# optical_flow.setUseInitialFlow(params['useInitialFlow'])
+	flow = optical_flow.calc(old_gray, frame_gray, None)
 
 	# Compute the magnitude and angle of the flow
 	mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
@@ -67,6 +105,8 @@ while 1:
 
 	# Now update the previous frame
 	old_gray = frame_gray.copy()
+
+	frame_number += 1
 
 cv2.destroyAllWindows()
 cap.release()
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
+#################################
+### Lucas-Kanade Optical Flow ###
+#################################
+
+# Import the required libraries
 import cv2
 import numpy as np
 
 print("Installed opencv version", cv2.__version__)
 print("Installed numpy version", np.__version__)
 
+#####################
+# Parameters
+#####################
+
 FEATURES = 100
 
-# params for ShiTomasi corner detection
+# Parameters for feature selection
+# See: https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga4055896d9ef77dd3cacf2c5f60e13f1c
 feature_params = dict(
-	maxCorners=FEATURES,
-	qualityLevel=0.3,
-	minDistance=20,
-	blockSize=7
+	maxCorners=FEATURES,  # Maximum number of features to track
+	qualityLevel=0.3,  # Minimal eigenvalue of the feature matrix
+	minDistance=20,  # Minimum distance between features
+	blockSize=7  # Dimension of the search matrix W
 )
 
-# Parameters for lucas kanade optical flow
+# Parameters for Lucas-Kanade optical flow
 lk_params = dict(
-	winSize=(30, 30),
-	maxLevel=0,
-	criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+	winSize=(30, 30),  # Dimension of the search matrix W
+	maxLevel=3,  # Maximum pyramid level
+	criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)  # Convergence criteria
 )
 
-vidPath = "data/level_1.mp4"
+#####################
+# Preparation
+#####################
+
+vidPath = "data/level_2.mp4"
 output_video_file = "output_video.mp4"
 
 cap = cv2.VideoCapture(vidPath)
@@ -44,21 +58,25 @@ p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
 
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
-etime = 0
+
+#####################
+# Tracking
+#####################
+
 while 1:
 	ret, frame = cap.read()
 	if frame is None:
 		break
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-	# calculate optical flow
+	# Calculate optical flow
 	p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
 	# Select good points
 	good_new = p1[st == 1]
 	good_old = p0[st == 1]
 
-	# draw the tracks
+	# Draw the tracks
 	for i, (new, old) in enumerate(zip(good_new, good_old)):
 		a, b = new.astype('int').ravel()
 		c, d = old.astype('int').ravel()
@@ -75,6 +93,7 @@ while 1:
 	old_gray = frame_gray.copy()
 	p0 = good_new.reshape(-1, 1, 2)
 
+# Close windows and release video capture and writer
 cv2.destroyAllWindows()
 cap.release()
 out.release()
